09cc10353a7e4460e93ab2d739aac49c1c8ce13e
Griffin Knipe <knipegp@gmail.com>
2021-05-10 08:27:26 -0400

    Finish structs

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -26,11 +26,11 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self) -> bool {
         // Something goes here...
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here... 
     }
 }
